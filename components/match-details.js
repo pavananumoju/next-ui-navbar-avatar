89b78cd86094cd47e66445300fdaf14ff8a5ac8a
Pavan Kumar Anumoju <pavananumoju@gmail.com>
2023-07-11 00:22:58 +0530

    refactoring: Team-Post-Card

--- components/match-details.js
+++ components/match-details.js
@@ -4,6 +4,7 @@ import { useAuth } from "@/context/auth-context";
 import { getDate_dd_month_yyyy, getDate_wwwddmmyyyy } from "./utils/date-utils";
 import { currentMatchDay } from "./utils/mock-utils";
 import MatchCardRow from "./match-card-row";
+import TeamPostCard from "./team-post-card";
 
 function MatchDetails(props) {
   // const router = useRouter();
@@ -28,14 +29,6 @@ function MatchDetails(props) {
           match2_team2: mdm.match[1] && mdm.match[1].matchInfo.team2.teamSName,
         }
       : null;
-
-  // function handleMatchClick(match, team1_id, team2_id) {
-  //   const dateObj = new Date(data.date);
-  //   const date_dd_month_yyyy = getDate_dd_month_yyyy(dateObj);
-  //   router.push(
-  //     `/function/team-selection-page/?date=${date_dd_month_yyyy}&match=${match}&team1=${team1_id}&team2=${team2_id}`
-  //   );
-  // }
 
   var today = getDate_wwwddmmyyyy(new Date());
 
@@ -84,108 +77,13 @@ function MatchDetails(props) {
           </Card>
           <Spacer y={1} />
           {match1_posts && (
-            <ul>
-              <Text color="warning">Match 1</Text>
-              {/* <Spacer x={0.5}/> */}
-              {match1_posts &&
-                match1_posts.map((post) => (
-                  <Grid.Container
-                    gap={1}
-                    key={`${post.email} + ${post.p1.id}`}
-                    sm="12"
-                  >
-                    <Card
-                      isPressable
-                      isHoverable
-                      variant="bordered"
-                      css={{
-                        mw: "600px",
-                        borderColor: "blue  ",
-                        // backgroundColor:"black"
-                      }}
-                    >
-                      <Card.Body>
-                        <Row align="center">
-                          <Col>
-                            <Text
-                              color={user.email === post.email ? "warning" : ""}
-                            >
-                              {user.email === post.email
-                                ? "Your team"
-                                : post.name}
-                            </Text>
-                            <Text>{post.updatedTime}</Text>
-                          </Col>
-                          <Col>
-                            <Text color={post.p1.mvp && "success"}>
-                              {post.p1.name}
-                            </Text>
-                            <Text color={post.p2.mvp && "success"}>
-                              {post.p2.name}
-                            </Text>
-                            <Text color={post.p3.mvp && "success"}>
-                              {post.p3.name}
-                            </Text>
-                          </Col>
-                        </Row>
-                      </Card.Body>
-                    </Card>
-                  </Grid.Container>
-                ))}
-            </ul>
+            <TeamPostCard posts={match1_posts} user={user} match={"Match 1"} />
           )}
 
           <Spacer y={1} />
 
           {match2_posts && (
-            <ul>
-              <Text color="warning">Match 2</Text>
-              {match2_posts &&
-                match2_posts.map((post) => (
-                  <Grid.Container
-                    gap={1}
-                    key={`${post.email} + ${post.p1.id}`}
-                    sm="12"
-                  >
-                    <Card
-                      isPressable
-                      isHoverable
-                      variant="bordered"
-                      css={{
-                        mw: "600px",
-                        borderColor: "violet",
-                        // backgroundColor:"black"
-                      }}
-                    >
-                      <Card.Body>
-                        <Row align="center">
-                          <Col>
-                            <Text
-                              color={user.email === post.email ? "warning" : ""}
-                            >
-                              {user.email === post.email
-                                ? "Your team"
-                                : post.name}
-                            </Text>
-                            <Text>{post.updatedTime}</Text>
-                          </Col>
-                          <Col>
-                            <Text color={post.p1.mvp && "success"}>
-                              {post.p1.name}
-                            </Text>
-                            <Text color={post.p2.mvp && "success"}>
-                              {post.p2.name}
-                            </Text>
-                            <Text color={post.p3.mvp && "success"}>
-                              {post.p3.name}
-                            </Text>
-                          </Col>
-                        </Row>
-                      </Card.Body>
-                    </Card>
-                  </Grid.Container>
-                ))}
-            </ul>
+            <TeamPostCard posts={match2_posts} user={user} match={"Match 2"} />
           )}
         </>
       )}
---
+++ components/team-post-card.js
@@ -0,0 +1,46 @@
+import { Spacer, Col, Card, Grid, Text, Button, Row } from "@nextui-org/react";
+
+function TeamPostCard(props) {
+
+const posts = props.posts;
+const user = props.user;
+
+  return (
+    <ul>
+      <Text color="warning">{props.match}</Text>
+      {posts &&
+        posts.map((post) => (
+          <Grid.Container gap={1} key={`${post.email} + ${post.p1.id}`} sm="12">
+            <Card
+              isPressable
+              isHoverable
+              variant="bordered"
+              css={{
+                mw: "600px",
+                borderColor: "violet",
+                // backgroundColor:"black"
+              }}
+            >
+              <Card.Body>
+                <Row align="center">
+                  <Col>
+                    <Text color={user.email === post.email ? "warning" : ""}>
+                      {user.email === post.email ? "Your team" : post.name}
+                    </Text>
+                    <Text>{post.updatedTime}</Text>
+                  </Col>
+                  <Col>
+                    <Text color={post.p1.mvp && "success"}>{post.p1.name}</Text>
+                    <Text color={post.p2.mvp && "success"}>{post.p2.name}</Text>
+                    <Text color={post.p3.mvp && "success"}>{post.p3.name}</Text>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Grid.Container>
+        ))}
+    </ul>
+  );
+}
+
+export default TeamPostCard;
