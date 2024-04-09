00cd1d155a828a11f2ff9f5f69b2c9045597311f
Pavan Kumar Anumoju <pavananumoju@gmail.com>
2024-04-09 21:30:29 +0530

    dynamic current date

--- components/utils/mock-utils.js
+++ components/utils/mock-utils.js
@@ -1,4 +1,5 @@
 import { getDate_wwwddmmyyyy } from "./date-utils";
 
-export const todaysMatchDate = new Date(2024, 3, 2);
+// export const todaysMatchDate = new Date(2024, 3, 9);
+export const todaysMatchDate = new Date();
 export const currentMatchDay = getDate_wwwddmmyyyy(todaysMatchDate);
